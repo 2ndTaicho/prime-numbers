1b0553616bc752c8eb58a8d1bcd22a536a6615e5
Sumaiyah Domingo <71407982+2ndTaicho@users.noreply.github.com>
2021-01-26 20:46:57 +0200

    primenumbers

---
+++ primes.js
@@ -0,0 +1,14 @@
+exports.primeNumbers = function () { 
+    for (var counter = 0; counter <= 100; counter++) {
+
+        var notPrime = false;
+            for (var i = 2; i <= counter; i++) {
+                if (counter%i===0 && i!==counter) {
+                    notPrime = true;
+                }
+            }
+            if (notPrime === false) {
+                return counter;
+            }
+    }
+}
